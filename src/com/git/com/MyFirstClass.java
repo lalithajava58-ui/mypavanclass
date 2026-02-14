c6b0a59bd2a23d0dd9f4a687df35be1632c79213
Lalitha <lalithajava58@gmail.com>
2026-02-13 21:28:16 -0500

    'hello'

---
+++ src/com/git/com/MyFirstClass.java
@@ -0,0 +1,18 @@
+package com.git.com;
+
+
+
+public class MyFirstClass {
+	
+
+public static void main(String[] args) {
+	
+	System.out.println("hi");
+}
+
+	   
+	    
+	}
+	
+
+
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MyGitHubJenkin_End_End {
+}
